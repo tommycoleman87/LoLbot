436b5fce72a828054799f9a1b58ade9402bb6c19
Tommy Coleman <tommiilee@gmail.com>
2020-10-25 21:39:53 -0400

    wip - api calls using cassiopeia

--- LoLbot.py
+++ LoLbot.py
@@ -3,32 +3,27 @@ import discord
 import requests
 from discord.ext import commands
 from dotenv import load_dotenv
-
+import cassiopeia as cass
 load_dotenv()
+
 
 LOL_TOKEN = os.getenv('LOL_TOKEN')
 TOKEN = os.getenv('DISCORD_TOKEN')
+
+cass.set_riot_api_key(LOL_TOKEN)  # This overrides the value set in your configuration/settings.
+cass.set_default_region("NA")
+
 bot = commands.Bot(command_prefix='!')
-riot_api = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'
-summoner_id = ''
-headers = {'X-Riot-Token' : f'{LOL_TOKEN}'}
+summoner = ''
 @bot.command(name='summoner')
 async def get_summoner(ctx, arg):
-    response = requests.get(riot_api + f'{arg}', headers=headers)
-    global summoner_id
-    summoner_id = response.json()['id']
-    if response.status_code == 200:
-        await ctx.send(f'Summoner set to {arg}')
-    else:
-        await ctx.send(f'Error {response.status_code}')
+   global summoner
+   summoner = cass.get_summoner(name=arg)
+   await ctx.send(f'{summoner.name} is level {summoner.level}')
 
 @bot.command(name='match')
 async def get_match_stats(ctx):
-    response = requests.get(f'https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/{summoner_id}', headers=headers )
-    print(summoner_id)
-    if response.status_code == 200:
-        await ctx.send(response.json())
-    else:
-        await ctx.send(f'Error {response.status_code}')
+    matches = summoner.match_history
+    await ctx.send(matches[0])
 
 bot.run(TOKEN)
